f3b3c751d8999b2e31f621334d623844bb6e8bed
SaltmineMP4 <112489634+SaltmineMP4@users.noreply.github.com>
2023-02-02 15:56:24 +0100

    wqewqe

---
+++ userProfileTest.js
@@ -0,0 +1,4 @@
+function profileCard(profileName, fullName, address, city, state, zip, phone, email) {
+    console.log(profileCard);
+}
+profileCard('Salt', 'Henk Bakker', '404 Sample St.', 'HomeTown', 'NH', '1234AB', '123-456-7890', 'sampleEmail@sampleProvider.null');
